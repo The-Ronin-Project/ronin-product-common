3961b373e894c1774e68ebf5aa89880f03a34a72
Jesse Corbett <jesse@projectronin.com>
2022-10-21 15:14:22 -0700

    Schema branch gen (#21)
    * Add the ability to generate a set of schema files from a git branch, for testing purposes
    
    * Add comments
    
    * Change dateTimeType to Instant

--- product-gradle-common/src/main/kotlin/com/projectronin/product/json-schema.gradle.kts
+++ product-gradle-common/src/main/kotlin/com/projectronin/product/json-schema.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     implementation("com.google.code.findbugs:jsr305:3.0.2")
 }
 
+/**
+ * The logic and configuration "store" for the plugin tasks
+ */
 abstract class JsonSchemaImportTask : DefaultTask() {
     private val schemaBaseUrl = "https://repo.devops.projectronin.io/repository/ronin-raw/"
 
@@ -31,8 +34,15 @@ abstract class JsonSchemaImportTask : DefaultTask() {
     @get:Input
     abstract val tag: Property<String>
 
+    @get:Input
+    abstract val gitUrl: Property<String>
+
+    @get:Input
+    abstract val gitBranch: Property<String>
+
     private val rootDir = project.file(project.buildDir.path + "/jsonschema/$name")
     private val sourcesDir = project.file(rootDir.path + "/sources")
+    private val gitRepoDir = project.file(rootDir.path + "/git")
 
     @get:OutputDirectory
     val generatedClasses: File = project.file(rootDir.path + "/generated")
@@ -69,7 +79,7 @@ abstract class JsonSchemaImportTask : DefaultTask() {
             includeToString = true
             includeAdditionalProperties = true
             dateType = "java.time.LocalDate"
-            dateTimeType = "java.time.LocalDateTime"
+            dateTimeType = "java.time.Instant"
             initializeCollections = true // true=collection initialized as empty collection
             includeJsr305Annotations = true
             fileFilter = FileFilter { file ->
@@ -82,6 +92,30 @@ abstract class JsonSchemaImportTask : DefaultTask() {
     @TaskAction
     fun fetchSchema() {
         rootDir.mkdirs()
+
+        if (gitUrl.isPresent && gitBranch.isPresent) {
+            fetchGitSchema()
+        } else {
+            fetchNexusSchema()
+        }
+
+        Jsonschema2Pojo.generate(config, GradleRuleLogger(project.logger))
+    }
+
+    private fun fetchGitSchema() {
+        gitRepoDir.deleteRecursively()
+        project.exec {
+            workingDir = rootDir
+            commandLine("git", "clone", "--depth", "1", gitUrl.get(), "--branch", gitBranch.get(), "--single-branch", gitRepoDir.name)
+        }
+
+        project.copy {
+            from(project.file(gitRepoDir.path + "/" + version.get()))
+            into(sourcesDir)
+        }
+    }
+
+    private fun fetchNexusSchema() {
         val artifactName = "${schema.get()}-${version.get()}-${tag.get()}.tar.gz"
         val artifactFile = project.file(rootDir.path + "/$artifactName")
 
@@ -93,21 +127,36 @@ abstract class JsonSchemaImportTask : DefaultTask() {
             from(project.tarTree(artifactFile))
             into(sourcesDir)
         }
-
-        Jsonschema2Pojo.generate(config, GradleRuleLogger(project.logger))
     }
 }
 
+/**
+ * The actual interface between the plugin and the consuming build.gradle file
+ */
 abstract class JsonSchemasExtension(private val project: Project) {
     fun schema(name: String, version: String, tag: String, namespace: String = "contract-event") {
-        val formattedName = Regex("-[a-zA-Z]").replace("generate-${namespace}-${name}-${version}-Classes") {
-            it.value.replace("-", "").toUpperCase()
-        }
-        project.tasks.create<JsonSchemaImportTask>(formattedName) {
+        project.tasks.create<JsonSchemaImportTask>(formatName(name, version, namespace)) {
             this.namespace.set(namespace)
             this.schema.set(name)
             this.version.set(version)
             this.tag.set(tag)
+        }
+    }
+
+    fun branchSchema(name: String, version: String, url: String, branch: String, namespace: String = "contact-event") {
+        project.tasks.create<JsonSchemaImportTask>(formatName(name, version, namespace)) {
+            this.namespace.set(namespace)
+            this.schema.set(name)
+            this.version.set(version)
+            this.tag.set("git-branch:$branch")
+            this.gitUrl.set(url)
+            this.gitBranch.set(branch)
+        }
+    }
+
+    private fun formatName(name: String, version: String, namespace: String): String {
+        return Regex("-[a-zA-Z]").replace("generate-${namespace}-${name}-${version}-Classes") {
+            it.value.replace("-", "").toUpperCase()
         }
     }
 }
