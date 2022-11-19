61db3ca08d0c0a776aa74b7461a3839b46617312
roninbrad <brad@projectronin.com>
2022-11-18 16:43:04 -0800

    DASH-3868 - moving the xample PatientClient into it's own file

---
+++ product-spring-common/src/test/kotlin/com/projectronin/product/common/client/examples/PatientClient.kt
@@ -0,0 +1,58 @@
+package com.projectronin.product.common.client.examples
+
+import com.projectronin.product.common.client.AbstractServiceClient
+import com.projectronin.validation.clinical.data.client.work.auth.AuthBroker
+import okhttp3.OkHttpClient
+import java.time.Instant
+import java.time.LocalDate
+
+// An example of a client implementation for Patient endpoints on the CDS Service.
+//     (obviously this would NOT actually live in the 'common' project, only here as an example)
+private const val PATIENT_PATH = "api/patient"
+class PatientClient(
+    hostUrl: String,
+    authBroker: AuthBroker,
+    client: OkHttpClient = defaultOkHttpClient()
+) :
+    AbstractServiceClient(hostUrl, authBroker, client) {
+    override fun getUserAgentValue(): String {
+        return "PatientClient/1.0.0"
+    }
+
+    fun get(id: String): Patient {
+        return executeGet("$baseUrl$PATIENT_PATH/$id")
+    }
+
+    fun create(patient: Patient): Patient {
+        return executePost("$baseUrl$PATIENT_PATH", patient)
+    }
+
+    fun delete(id: String) {
+        executeRawDelete("$baseUrl$PATIENT_PATH/$id")
+    }
+}
+
+//
+// arguably more ideal to reference a class from the
+//   CDS Service Patient rather than make our own 'identical copy of the object
+//     But this is used for demonstration purposes.
+//        (this class is 'oversimplified' and would be constructed different IRL)
+data class Patient(
+    val id: String = "",
+    val tenantId: String = "",
+    val active: Boolean = true,
+    val mrn: String = "",
+    val udpId: String = "",
+    val displayName: String = "",
+    val birthSex: String = "",
+    val birthDate: LocalDate? = null,
+    val telecoms: List<PatientTelecom> = listOf(),
+    val createdAt: Instant? = null,
+    val updatedAt: Instant? = null
+)
+
+data class PatientTelecom(
+    val telecomSystem: String = "",
+    val telecomUse: String = "",
+    val telecomValue: String = ""
+)
--- product-spring-common/src/test/kotlin/com/projectronin/product/common/client/examples/PatientDemoTest.kt
+++ product-spring-common/src/test/kotlin/com/projectronin/product/common/client/examples/PatientDemoTest.kt
@@ -1,15 +1,11 @@
 package com.projectronin.product.common.client.examples
 
-import com.projectronin.product.common.client.AbstractServiceClient
-import com.projectronin.validation.clinical.data.client.work.auth.AuthBroker
 import com.projectronin.validation.clinical.data.client.work.auth.PassThruAuthBroker
 import com.projectronin.validation.clinical.data.client.work.exception.ServiceClientException
-import okhttp3.OkHttpClient
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
-import java.time.Instant
 import java.time.LocalDate
 
 private const val CDS_URL = "https://clinical-data.dev.projectronin.io"
@@ -60,56 +56,3 @@ class PatientDemoTest {
         }
     }
 }
-
-// /////////////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////////////
-// Below is how to implement a CDS Patient Client to be used for REST calls on Patient endpoint
-//     This is for demo purposes and would ultimately find a 'better home'
-//
-private const val PATIENT_PATH = "api/patient"
-class PatientClient(
-    hostUrl: String,
-    authBroker: AuthBroker,
-    client: OkHttpClient = defaultOkHttpClient()
-) :
-    AbstractServiceClient(hostUrl, authBroker, client) {
-    override fun getUserAgentValue(): String {
-        return "PatientClient/1.0.0"
-    }
-
-    fun get(id: String): Patient {
-        return executeGet("$baseUrl$PATIENT_PATH/$id")
-    }
-
-    fun create(patient: Patient): Patient {
-        return executePost("$baseUrl$PATIENT_PATH", patient)
-    }
-
-    fun delete(id: String) {
-        executeRawDelete("$baseUrl$PATIENT_PATH/$id")
-    }
-}
-
-// arguably more ideal to reference a class from the
-//   CDS Service Patient rather than make our own 'identical copy of the object
-//     But this is used for demonstration purposes.
-//        (this class is 'oversimplified' and would be constructed different IRL)
-data class Patient(
-    val id: String = "",
-    val tenantId: String = "",
-    val active: Boolean = true,
-    val mrn: String = "",
-    val udpId: String = "",
-    val displayName: String = "",
-    val birthSex: String = "",
-    val birthDate: LocalDate? = null,
-    val telecoms: List<PatientTelecom> = listOf(),
-    val createdAt: Instant? = null,
-    val updatedAt: Instant? = null
-)
-
-data class PatientTelecom(
-    val telecomSystem: String = "",
-    val telecomUse: String = "",
-    val telecomValue: String = ""
-)
