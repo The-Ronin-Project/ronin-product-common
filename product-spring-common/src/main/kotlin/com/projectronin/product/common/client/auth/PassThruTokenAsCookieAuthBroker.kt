1ef404022c1b42b24b6827a43a6f852fff61e375
roninbrad <brad@projectronin.com>
2022-12-01 15:51:04 -0800

    DASH-3868 - add an alternate way to auth via Cookie w/ token

---
+++ product-spring-common/src/main/kotlin/com/projectronin/product/common/client/auth/PassThruTokenAsCookieAuthBroker.kt
@@ -0,0 +1,18 @@
+package com.projectronin.product.common.client.auth
+
+import org.springframework.http.HttpHeaders
+import java.time.Instant
+
+class PassThruTokenAsCookieAuthBroker(val authToken: String, val state: Long = Instant.now().epochSecond) : AuthBroker {
+
+    private val authHeaderMap : Map<String,String>
+    init {
+        authHeaderMap = mapOf(
+            HttpHeaders.COOKIE to "user_session_token_${state}=$authToken",
+            "X-STATE" to "$state")
+    }
+
+    override fun generateAuthHeaders(): Map<String, String> {
+        return authHeaderMap
+    }
+}
