e1a5c19a67cacdae4d57673878b0ac076e394e84
David Dionise <ddionise@projectronin.com>
2023-08-03 13:20:04 -0400

    fixed multiple bean issue with LD client (#97)
    * fixed multiple bean issue with LD client
    
    * removed unused import
    
    ---------
    
    Co-authored-by: David Dionise <ddionoise@projectronin.com>

--- product-spring-modules/product-spring-launchdarkly/src/main/kotlin/com/projectronin/product/launchdarkly/config/LaunchDarklyConfig.kt
+++ product-spring-modules/product-spring-launchdarkly/src/main/kotlin/com/projectronin/product/launchdarkly/config/LaunchDarklyConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 
 @AutoConfiguration
 @EnableConfigurationProperties(LaunchDarklyProperties::class)
@@ -30,6 +31,7 @@ open class LaunchDarklyConfig {
 
     @Bean
     @ConditionalOnProperty("ronin.product.launch-darkly.client-sdk-key")
+    @Primary
     open fun launchDarklyClient(ldProps: LaunchDarklyProperties): LDClient {
         val config = LDConfig.Builder().run {
             offline(ldProps.offline)
