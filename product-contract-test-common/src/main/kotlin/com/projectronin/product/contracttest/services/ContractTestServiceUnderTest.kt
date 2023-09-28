27db18a8b069c66b49cb91fc6e80c25a5554e893
rossLodge-R <110425236+rossLodge-R@users.noreply.github.com>
2023-09-28 13:18:19 -0700

    DASH-5679: Update contract test plugin to enable coverage (#112)
    * DASH-5679: Update contract test plugin to enable coverage
    
    * Update the file name search

--- product-contract-test-common/src/main/kotlin/com/projectronin/product/contracttest/services/ContractTestServiceUnderTest.kt
+++ product-contract-test-common/src/main/kotlin/com/projectronin/product/contracttest/services/ContractTestServiceUnderTest.kt
@@ -13,7 +13,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.test.util.TestSocketUtils
 import java.io.File
+import java.lang.management.ManagementFactory
 import java.util.Properties
+import java.util.UUID
 
 /**
  * Runs a spring boot app from the `build/libs` directory.  Meant to start up the service you're trying to contract-test, and thus must be provided with a list
@@ -28,9 +30,13 @@ import java.util.Properties
  */
 class ContractTestServiceUnderTest(
     override val dependentServices: List<ContractTestService>,
-    val jarDirectorySubPath: String = "libs",
-    val testConfigResourceName: String = "application-test.properties",
-    val debugArguments: String? = null
+    private val jarDirectorySubPath: String = "libs",
+    private val testConfigResourceName: String = "application-test.properties",
+    private val debug: Boolean = false,
+    private val debugPort: Int = 5005,
+    private val debugSuspend: Boolean = true,
+    private val additionalArguments: List<String> = emptyList(),
+    private val environmentVariables: Map<String, String> = emptyMap()
 ) : ContractTestService {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
@@ -52,67 +58,11 @@ class ContractTestServiceUnderTest(
             if (process == null) {
                 logger.info("Starting service under test")
 
-                val tempApplicationPropertiesFile: File = run {
-                    val f = File.createTempFile("contract-test", testConfigResourceName.replace(""".*\.""".toRegex(), "."))
-                    f.deleteOnExit()
-                    f
-                }
+                val testSetupData = configureTestDirectoriesAndFiles()
 
-                val props = Properties()
-                props.load(LocalContractTestExtension::class.java.classLoader.getResourceAsStream("test.properties"))
+                configureSpringConfigFile(testSetupData)
 
-                val projectBuildDir: File = File(props.getProperty("project.build.dir"))
-                val projectDir: File = File(props.getProperty("project.dir"))
-
-                val logOutputDir = File(projectBuildDir, "contract-test-service-logs")
-                if (!logOutputDir.exists()) {
-                    logOutputDir.mkdirs()
-                }
-
-                when (val resource = LocalContractTestExtension::class.java.classLoader.getResource(testConfigResourceName)) {
-                    null -> fail("Must have a $testConfigResourceName on the localContractTest classpath")
-                    else -> {
-                        val importedText = (dependentServices.flatMap { it.replacementTokens.entries.map { entry -> entry.key to entry.value } } + setOf("servicePort" to servicePort.toString()))
-                            .fold(resource.readText()) { text, entry ->
-                                text.replace("{{${entry.first}}}", entry.second)
-                            }
-                        tempApplicationPropertiesFile.writeText(
-                            """
-                            |$importedText
-                            |${if (testConfigResourceName.endsWith(".properties")) "server.port=$servicePort" else "server:\n  port: $servicePort"}
-                            """.trimMargin("|")
-                        )
-                    }
-                }
-
-                logger.info("Log output will be available at $logOutputDir")
-
-                process = run {
-                    val javaCommand = "${System.getProperty("java.home")}/bin/java"
-                    when (
-                        val jarFile = (File(projectBuildDir, jarDirectorySubPath).listFiles() ?: arrayOf<File>())
-                            .firstOrNull { f -> f.extension == "jar" && !f.name.contains("javadoc|sources|plain".toRegex()) }
-                    ) {
-                        null -> fail("Must be able to find spring boot jar")
-                        else -> {
-                            val commands = mutableListOf<String>()
-                            commands += javaCommand
-                            if (debugArguments != null) {
-                                commands += debugArguments
-                            }
-                            commands += listOf(
-                                "-jar",
-                                jarFile.absolutePath,
-                                "--spring.config.location=${tempApplicationPropertiesFile.absolutePath}"
-                            )
-                            ProcessBuilder(*commands.toTypedArray())
-                                .directory(projectDir)
-                                .redirectOutput(File(logOutputDir, "stdout.log"))
-                                .redirectError(File(logOutputDir, "stderr.log"))
-                                .start()
-                        }
-                    }
-                }
+                process = startProcess(testSetupData, constructProcessArguments(testSetupData, debug, debugPort, debugSuspend, additionalArguments), environmentVariables)
 
                 runBlocking {
                     runCatching {
@@ -121,11 +71,118 @@ class ContractTestServiceUnderTest(
                         }
                     }
                         .onFailure { t ->
-                            throw RuntimeException("Failed to verify service: ${File(logOutputDir, "stdout.log").readText()}", t)
+                            throw RuntimeException("Failed to verify service: ${File(testSetupData.logOutputDir, "stdout.log").readText()}", t)
                         }
                 }
             }
         }
+    }
+
+    private fun constructProcessArguments(
+        setupData: TestSetupData,
+        debug: Boolean,
+        debugPort: Int,
+        debugSuspend: Boolean,
+        additionalArguments: List<String>
+    ): List<String> {
+        val processArguments = mutableListOf<String>()
+        processArguments += additionalArguments
+        if (debug) {
+            processArguments += "-agentlib:jdwp=transport=dt_socket,server=y,suspend=${if (debugSuspend) "y" else "n"},address=*:$debugPort"
+        }
+
+        val runtimeMxBean = ManagementFactory.getRuntimeMXBean()
+        val arguments = runtimeMxBean.inputArguments
+
+        val ideaArguments = arguments.filter { it.matches("""-D.*coverage.*""".toRegex()) }
+        val javaAgentArgument = arguments.firstOrNull { it.matches("""-javaagent.*?(intellij-coverage-agent.*?\.jar|jacocoagent.jar).*""".toRegex()) }
+
+        if (javaAgentArgument != null) {
+            val sourceJarLocation = File(javaAgentArgument.replace(".*-javaagent:(.*?(intellij-coverage-agent.*?\\.jar|jacocoagent.jar)).*".toRegex(), "$1"))
+            val destinationJarLocation = run {
+                val tf = File.createTempFile("coverage-agent", ".jar")
+                tf.deleteOnExit()
+                sourceJarLocation.copyTo(tf, overwrite = true)
+                tf
+            }
+
+            val newJavaAgentArgument = javaAgentArgument
+                .replace("-javaagent:.*?(intellij-coverage-agent.*?\\.jar|jacocoagent.jar)".toRegex(), "-javaagent:$destinationJarLocation")
+                .replace("build/jacoco/.*?\\.exec".toRegex(), "${setupData.projectBuildDir.absolutePath}/jacoco/test-${UUID.randomUUID()}.exec")
+
+            processArguments += newJavaAgentArgument
+            processArguments += ideaArguments
+        }
+
+        return processArguments
+    }
+
+    private fun startProcess(testSetupData: TestSetupData, additionalArguments: List<String>, environmentVariables: Map<String, String>): Process? {
+        val javaCommand = "${System.getProperty("java.home")}/bin/java"
+        return when (
+            val jarFile = (File(testSetupData.projectBuildDir, jarDirectorySubPath).listFiles() ?: arrayOf<File>())
+                .firstOrNull { f -> f.extension == "jar" && !f.name.contains("javadoc|sources|plain".toRegex()) }
+        ) {
+            null -> fail("Must be able to find spring boot jar")
+            else -> {
+                val commands = mutableListOf<String>()
+                commands += javaCommand
+                commands += additionalArguments
+                commands += listOf(
+                    "-jar",
+                    jarFile.absolutePath,
+                    "--spring.config.location=${testSetupData.tempApplicationPropertiesFile.absolutePath}"
+                )
+                val processBuilder = ProcessBuilder(*commands.toTypedArray())
+                    .directory(testSetupData.projectDir)
+                    .redirectOutput(File(testSetupData.logOutputDir, "stdout.log"))
+                    .redirectError(File(testSetupData.logOutputDir, "stderr.log"))
+
+                processBuilder.environment() += environmentVariables
+
+                processBuilder
+                    .start()
+            }
+        }
+    }
+
+    private fun configureSpringConfigFile(testSetupData: TestSetupData) {
+        when (val resource = LocalContractTestExtension::class.java.classLoader.getResource(testConfigResourceName)) {
+            null -> fail("Must have a $testConfigResourceName on the localContractTest classpath")
+            else -> {
+                val importedText = (dependentServices.flatMap { it.replacementTokens.entries.map { entry -> entry.key to entry.value } } + setOf("servicePort" to servicePort.toString()))
+                    .fold(resource.readText()) { text, entry ->
+                        text.replace("{{${entry.first}}}", entry.second)
+                    }
+                testSetupData.tempApplicationPropertiesFile.writeText(
+                    """
+                    |$importedText
+                    |${if (testConfigResourceName.endsWith(".properties")) "server.port=$servicePort" else "server:\n  port: $servicePort"}
+                    """.trimMargin("|")
+                )
+            }
+        }
+    }
+
+    private fun configureTestDirectoriesAndFiles(): TestSetupData {
+        val props = Properties()
+        props.load(LocalContractTestExtension::class.java.classLoader.getResourceAsStream("test.properties"))
+
+        return TestSetupData(
+            tempApplicationPropertiesFile = run {
+                val f = File.createTempFile("contract-test", testConfigResourceName.replace(""".*\.""".toRegex(), "."))
+                f.deleteOnExit()
+                f
+            },
+            projectBuildDir = File(props.getProperty("project.build.dir")),
+            projectDir = File(props.getProperty("project.dir")),
+            logOutputDir = File(File(props.getProperty("project.build.dir")), "contract-test-service-logs").apply {
+                if (!exists()) {
+                    mkdirs()
+                }
+                logger.info("Log output will be available at $this")
+            }
+        )
     }
 
     private fun verifyService() {
@@ -147,4 +204,11 @@ class ContractTestServiceUnderTest(
             }
         }
     }
+
+    private class TestSetupData(
+        val tempApplicationPropertiesFile: File,
+        val projectBuildDir: File,
+        val projectDir: File,
+        val logOutputDir: File
+    )
 }
